93a2c46cef52ced7b438ae3e2ee08bd43779e97d
muralimupparaju <muralimupparaju@gmail.com>
2025-02-26 21:25:14 -0500

    added two files

---
+++ iam.tf
@@ -0,0 +1,38 @@
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2-s3-cloudwatch-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+    }]
+  })
+}
+
+resource "aws_iam_policy" "s3_cloudwatch_policy" {
+  name        = "s3-cloudwatch-access-policy"
+  description = "Allow EC2 access to S3 and CloudWatch"
+  
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = ["s3:PutObject", "s3:GetObject", "s3:ListBucket"]
+        Resource = ["arn:aws:s3:::devops-bucket", "arn:aws:s3:::devops-bucket/*"]
+      },
+      {
+        Effect   = "Allow"
+        Action   = ["cloudwatch:PutMetricData"]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_policy" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.s3_cloudwatch_policy.arn
+}
